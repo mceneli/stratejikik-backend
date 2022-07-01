a6f84196defaf2725e7cdc15cd2b3db57d6a9216
mceneli <muhammetceneli@gmail.com>
2022-07-01 15:45:40 +0300

    login debug log

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
+const bcrypt = require("bcryptjs");
+const Joi = require("joi");
 
 router.route('/').get((req, res) => {
   User.find()
@@ -17,5 +19,38 @@ router.route('/add').post((req, res) => {
     .then(() => res.json('User added!'))
     .catch(err => res.status(400).json('Error: ' + err));
 });
+
+router.route('/login').post(async(req, res) => {
+ console.log("login metodu");
+ try{
+  const { error } = validate(req.body);
+  if (error)
+   return res.status(400).send({ message: error.details[0].message });
+
+ const user = await User.findOne({ username: req.body.username });
+  if (!user)
+   return res.status(401).send({ message: "Invalid Email or Password" }); 
+
+ const validPassword = await bcrypt.compare(
+  req.body.password,
+  user.password
+ );
+ if (!validPassword)
+  return res.status(401).send({ message: "Invalid Email or Password" });
+
+ const token = user.generateAuthToken();
+  res.status(200).send({ data: token, message: "logged in successfully" });
+ }catch (error) {
+  res.status(500).send({ message: "Internal Server Error" });
+ }
+});
+
+const validate = (data) => {
+	const schema = Joi.object({
+		username: Joi.string().username().required().label("Username"),
+		password: Joi.string().required().label("Password"),
+	});
+	return schema.validate(data);
+};
 
 module.exports = router;
